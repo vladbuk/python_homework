54989d2d920fde8af0db9d738a2a031fe79eb82e
vladbuk <vladbuk@gmail.com>
2022-09-13 20:11:33 +0300

    wip

--- practice/main_hometask.py
+++ practice/main_hometask.py
@@ -6,23 +6,26 @@
 # so a,b,c will be entered from user input 
 # (don't forget to implement messages for user, who will use this script).
 
-from dis import dis
-
 
 def main():
     # main() here our program will be started, 
     # and all required data asked
-    pass
+    a, b, c = ask_value("Enter 3 values of a, b, c backspace devided\n")
+    print("a, b, c : ", a, b, c)
+    print("discriminant: ", discriminant(a, b, c))
 
-def ask_value():
+def ask_value(message):
     # ask_value(message) this function should ask user for input, 
     # and then return variable. 
     # Good to have additional input validation here.
-    print("Input a"); a = int(input())
-    print("Input b"); b = int(input())
-    print("Input c"); c = int(input())
-    print(a, b, c)
-    print("Discriminant= ", discriminant(a, b, c))
+    a, b, c = input(message).split()
+    return int(a), int(b), int(c)
+    
+    #print("Input a"); a = int(input())
+    #print("Input b"); b = int(input())
+    #print("Input c"); c = int(input())
+    #print(a, b, c)
+    #print("Discriminant= ", discriminant(a, b, c))
 
 def discriminant(a, b, c):
     # discriminant(a,b,c) no much, no less just return discriminant.
@@ -33,9 +36,9 @@ def roots(d, a, b, c):
     # roots(d,a,b,c) will show on the screen all acceptable roots
     pass
 
-def solv_square():
+def solv_square(a, b, c):
     # solv_square(a,b,c) this function should 
     # contain inside discriminant and roots functions.
     pass
 
-ask_value()
+main()
