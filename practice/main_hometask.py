f0b3cdc10bf4854f8eee27926e1d5754dd4eac60
vladbuk <vladbuk@gmail.com>
2022-09-15 12:12:43 +0300

    final

--- practice/main_hometask.py
+++ practice/main_hometask.py
@@ -6,44 +6,44 @@
 # so a,b,c will be entered from user input 
 # (don't forget to implement messages for user, who will use this script).
 
+import sys
 
 def main():
     # main() here our program will be started, 
     # and all required data asked
-    a, b, c = ask_value("Enter 3 values of a, b, c backspace devided\n")
-    print("a, b, c : ", a, b, c)
-    print("Results: ", solv_square(a, b, c))
+    a, b, c = ask_value("Enter 3 int values of a, b, c devided by backspace\n")
+    solv_square(a, b, c)
 
 def ask_value(message):
     # ask_value(message) this function should ask user for input, 
     # and then return variable. 
     # Good to have additional input validation here.
-    a, b, c = input(message).split()
+    try:
+        a, b, c = input(message).split()
+    except:
+        print("We need three vars")
+        sys.exit()
+    
+    print("a b c = ", a, b ,c)
+    
+    if int(a) == 0:
+        print("var \"a\" mustn't be equal zero")
+        sys.exit()
+
     return int(a), int(b), int(c)
     
-    #print("Input a"); a = int(input())
-    #print("Input b"); b = int(input())
-    #print("Input c"); c = int(input())
-    #print(a, b, c)
-    #print("Discriminant= ", discriminant(a, b, c))
-
 def discriminant(a, b, c):
     # discriminant(a,b,c) no much, no less just return discriminant.
-    d = b**2 - 4 * a * c
+    d = b**2 - 4*a*c
     return d
 
-def roots():
-#def roots(d, a, b, c):
+#def roots():
+def roots(d, a, b, c):
     # roots(d,a,b,c) will show on the screen all acceptable roots
-    #a, b, c = ask_value("Enter 3 values of a, b, c backspace devided\n")
-    a, b, c = (1, 6, 9)
-    d = discriminant(a, b, c)
-    print(d)
+    print("Discriminant= {}".format(d))
     if d > 0:
-        root1 = (-b + (d ** 0.5) ) / (2 * a)
-        print(root1)
-        root2 = (-b - (d ** 0.5) ) / (2 * a)
-        print(root2)
+        root1 = ( -b + (d ** 0.5) ) / (2 * a)
+        root2 = ( -b - (d ** 0.5) ) / (2 * a)
         print("root1 = {}, root2 = {}".format(root1, root2))
     elif d == 0:
         root1 = root2 = -b / (2 * a)
@@ -51,7 +51,6 @@ def roots():
     else:
         real = -b / (2 * a)
         imaginary = -d ** 0.5 / (2 * a)
-        #print(real, imaginary)
         print("Roots are imaginary - {} plus-minus {} i".format(real, imaginary))
 
 def solv_square(a, b, c):
@@ -59,5 +58,4 @@ def solv_square(a, b, c):
     # contain inside discriminant and roots functions.
     return roots(discriminant(a, b, c), a, b, c)
 
-#main()
-roots()
+main()
