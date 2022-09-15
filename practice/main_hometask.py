c7f84aa989dd1cd89290b130df7554a2e1e4674d
vladbuk <vladbuk@gmail.com>
2022-09-15 12:31:58 +0300

    cleaned

--- practice/main_hometask.py
+++ practice/main_hometask.py
@@ -14,6 +14,7 @@ def main():
     a, b, c = ask_value("Enter 3 int values of a, b, c devided by backspace\n")
     solv_square(a, b, c)
 
+
 def ask_value(message):
     # ask_value(message) this function should ask user for input, 
     # and then return variable. 
@@ -24,38 +25,39 @@ def ask_value(message):
         print("We need three vars")
         sys.exit()
     
-    print("a b c = ", a, b ,c)
-    
     if int(a) == 0:
         print("var \"a\" mustn't be equal zero")
         sys.exit()
 
     return int(a), int(b), int(c)
-    
+
+
 def discriminant(a, b, c):
     # discriminant(a,b,c) no much, no less just return discriminant.
     d = b**2 - 4*a*c
     return d
 
-#def roots():
+
 def roots(d, a, b, c):
     # roots(d,a,b,c) will show on the screen all acceptable roots
     print("Discriminant= {}".format(d))
     if d > 0:
         root1 = ( -b + (d ** 0.5) ) / (2 * a)
         root2 = ( -b - (d ** 0.5) ) / (2 * a)
-        print("root1 = {}, root2 = {}".format(root1, root2))
+        print("root1 = {}, root2 = {}".format(int(root1), int(root2)))
     elif d == 0:
         root1 = root2 = -b / (2 * a)
-        print("root1 == root2 = {}".format(root1))
+        print("root1 == root2 = {}".format(int(root1)))
     else:
         real = -b / (2 * a)
         imaginary = -d ** 0.5 / (2 * a)
         print("Roots are imaginary - {} plus-minus {} i".format(real, imaginary))
 
+
 def solv_square(a, b, c):
     # solv_square(a,b,c) this function should 
     # contain inside discriminant and roots functions.
     return roots(discriminant(a, b, c), a, b, c)
+
 
 main()
